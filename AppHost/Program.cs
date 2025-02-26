7bd166576cfcd2bf59103e0c20f5b4c55469fc84
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2025-02-26 22:10:06 +0300

    Make aspire mogodb lifetime persistent

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -3,6 +3,7 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var mongoDb = builder.AddMongoDB("MongoDb")
+    .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume()
     .WithMongoExpress()
     .AddDatabase("DefaultDatabase");
